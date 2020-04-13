563374f673d9757c86b768887ed4aaa275452ef1
welcomecurry <bhandalamit@gmail.com>
2020-04-12 19:41:55 -0700

    Updates

---
+++ precache-manifest.5d51284920b42f0a85a7a2fcf524cd4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea745b747b354d1a6ee0cead2305d05",
+    "url": "/COVID19_Tracker/index.html"
+  },
+  {
+    "revision": "30c0879cf4acbf8ad938",
+    "url": "/COVID19_Tracker/static/css/main.42980aad.chunk.css"
+  },
+  {
+    "revision": "7dbf9c091b72ee2526db",
+    "url": "/COVID19_Tracker/static/js/2.79f96b8f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID19_Tracker/static/js/2.79f96b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c0879cf4acbf8ad938",
+    "url": "/COVID19_Tracker/static/js/main.90230ae3.chunk.js"
+  },
+  {
+    "revision": "e3c30994c5caec76f50d",
+    "url": "/COVID19_Tracker/static/js/runtime-main.f05a26a4.js"
+  }
+]);
